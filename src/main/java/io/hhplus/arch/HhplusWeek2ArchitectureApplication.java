bbe001a8921342ae427718196fd0358f199c66d7
hyunn12 <hyunn964@gmail.com>
2024-09-29 19:56:56 +0900

    Initial commit

---
+++ src/main/java/io/hhplus/arch/HhplusWeek2ArchitectureApplication.java
@@ -0,0 +1,13 @@
+package io.hhplus.arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusWeek2ArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusWeek2ArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/hhplus/arch/HhplusWeek2ArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.arch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusWeek2ArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
